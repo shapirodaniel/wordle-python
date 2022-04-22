cc305d69f724f5a931424ede452abce4cf496e97
shapirodaniel <shapirodanieladam@gmail.com>
2022-04-22 17:48:38 -0500

    update: duplicates check, play again logic

--- wordle.py
+++ wordle.py
@@ -1,5 +1,6 @@
 from words import words
 from random import random
+from copy import deepcopy
 
 
 class bcolors:
@@ -30,10 +31,24 @@ def select_random_word(words):
     return words[int(len(words) * random())]
 
 
+def get_letter_distribution(word):
+    result = {}
+    char_array = list(word)
+    for char in char_array:
+        try:
+            result[char] += 1
+        except KeyError:
+            result[char] = 1
+    return result
+
+
 def initialize_game():
+    answer = select_random_word(words)
+    letter_distribution = get_letter_distribution(answer)
+    print(letter_distribution)
     return {
-        "answer": select_random_word(words),
-        "num_guesses": 0,
+        "answer": answer,
+        "letter_distribution": letter_distribution,
         "guesses": [
             [
                 {"value": "_", "color": None},
@@ -43,19 +58,20 @@ def initialize_game():
                 {"value": "_", "color": None},
             ]
         ],
-        "won": False,
-        "playing": True,
     }
 
 
 def evaluate_guess(game_state, guess):
     result = []
+    letter_distribution = deepcopy(game_state["letter_distribution"])
     for idx, char in enumerate(list(guess.upper())):
         if game_state["answer"][idx] == char:
             result.append({"value": char.upper(), "color": bcolors.GREEN})
+            letter_distribution[char] -= 1
             continue
-        if game_state["answer"][idx] != char and char in game_state["answer"]:
+        elif char in game_state["answer"] and letter_distribution[char] > 0:
             result.append({"value": char.upper(), "color": bcolors.YELLOW})
+            letter_distribution[char] -= 1
             continue
         else:
             result.append({"value": char.upper(), "color": None})
@@ -77,40 +93,38 @@ def get_current_guess(game_state):
 
 def game_loop():
     print(f"welcome to {bcolors.GREEN}python wordle!{bcolors.ENDC}\n")
-
     game_state = initialize_game()
-
-    while game_state["playing"] == True:
+    while True:
         print(f"current guess: {get_current_guess(game_state)}\n")
         guess = input()
-
-        # error handling
         while len(guess) != 5:
             print("all wordle words are 5 letters! try again...")
             guess = input()
-
         result = evaluate_guess(game_state, guess)
         game_state["guesses"].append(result)
-
         if all([x["color"] == bcolors.GREEN for x in result]):
-            game_state["won"] = True
             print(f"you win, the word was {get_current_guess(game_state)}!")
-            exit(0)
-        elif len(game_state["guesses"]) > 6:
-            print("you lose :/ play again? type 1 for yes, 0 for no...")
-
+            print("play again? type 1 for yes, 0 for no ...")
             play_again = input()
-
             while play_again != "0" and play_again != "1":
                 print("type 1 to play again or 0 to exit wordle...")
                 play_again = input()
-
             if play_again == "1":
                 game_state = initialize_game()
                 continue
-
             else:
-                game_state["playing"] = False
+                print("thanks for playing wordle!")
+                exit(0)
+        elif len(game_state["guesses"]) > 6:
+            print("you lose :/ play again? type 1 for yes, 0 for no...")
+            play_again = input()
+            while play_again != "0" and play_again != "1":
+                print("type 1 to play again or 0 to exit wordle...")
+                play_again = input()
+            if play_again == "1":
+                game_state = initialize_game()
+                continue
+            else:
                 print("thanks for playing wordle!")
                 exit(0)
 
